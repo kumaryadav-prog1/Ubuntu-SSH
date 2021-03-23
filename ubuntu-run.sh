11540dc112f0fdea0b10967e07fb50b7c8b0ef09
kumaryadav-prog1 <77650981+kumaryadav-prog1@users.noreply.github.com>
2021-03-23 19:59:06 +0530

    Prevent Spam

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -1,13 +1,13 @@
 echo this can run commands in terminal tho
 
-wget https://github.com/xiaoxindada/SGSI-build-tool/releases/download/v11-1.4/SGSI-build-tool-11.tar
-tar -xf SGSI-build-tool-11.tar
+wget https://github.com/xiaoxindada/SGSI-build-tool/releases/download/v11-1.4/SGSI-build-tool-11.tar >> /dev/null
+tar -xf SGSI-build-tool-11.tar >> /dev/null
 rm SGSI-build-tool-11.tar
 echo 'Setup has been done Successfully'
 echo '--------------------------------'
 echo 'Getting SuperR ready for you'
-wget https://srkfree.sr-code.com/download/SuperRs-Kitchen_Linux-64_v1.2.1.2.zip
+wget https://srkfree.sr-code.com/download/SuperRs-Kitchen_Linux-64_v1.2.1.2.zip >> /dev/null
 mkdir SuperR
-unzip SuperRs-Kitchen_Linux-64_v1.2.1.2.zip -d SuperR/
+unzip SuperRs-Kitchen_Linux-64_v1.2.1.2.zip -d SuperR/ >> /dev/null
 rm SuperRs-Kitchen_Linux-64_v1.2.1.2.zip
 echo ' Job Done '
